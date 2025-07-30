144452c1935a6f42316434fa998497ae3b5681ac
dariusborca03 <borcadarius03@gmail.com>
2025-07-30 19:16:13 +0300

    Am incercat sa introduc Google Cloud Vision, dar nu am reusit e cv ft complicat

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,9 @@ use \Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiNomenclatureController;
 use App\Http\Controllers\ApiOfferController;
 use App\Http\Controllers\ApiPolicyController;
+use App\Http\Controllers\VehicleController;
 
+Route::post('/vehicles/process-registration-document', [VehicleController::class, 'processRegistrationDocument']);
 Route::get('/countries', [ApiNomenclatureController::class, 'getCountries']);
 Route::get('/counties', [ApiNomenclatureController::class, 'getCounties']);
 Route::get('/cities/{county_code}', [ApiNomenclatureController::class, 'getCities']);
